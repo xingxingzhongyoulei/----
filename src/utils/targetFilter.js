e3de3511b74653cda51b153e275fde857963d920
星星会笑滴 <2017759843@qq.com>
2024-08-18 16:29:34 +0800

    准备测试多重筛选

--- src/utils/targetFilter.js
+++ src/utils/targetFilter.js
@@ -8,6 +8,12 @@ export default class targetFilter {
     this.layer = null
     this.MultiTarget = null
     this.data = null
+    // 筛选数组
+    this.typeArr = []
+    this.speedArr = []
+    this.durArr = []
+    this.sizeArr = []
+
     this.initLayer()
     this.getTargetData().then(() => {
       this.addToMarker(this.filterData)
@@ -49,60 +55,123 @@ export default class targetFilter {
   filterMarker(type, val) {
     // 每回多选发生改变，执行清除图层所有图形
     this.clearMarker()
-    console.log(type)
-    console.log(val)
+    // console.log(type)
+    // console.log(val)
+    // let typeArr = []
+    // let speedArr = []
+    // let durArr = []
+    // let sizeArr = []
 
-    // switch (type) {
-    //   case 'type':
-    //     this.typeFilter(val)
-    //     break
-    //   case 'speed':
-    //     this.speedFilter(val)
-    //     break
-    //   case 'dur':
-    //     this.durFilter(val)
-    //     break
-    //   case 'size':
-    //     this.sizeFilter(val)
-    //     break
-    //   default:
-    //     break
-    // }
+    switch (type) {
+      case 'type':
+        this.typeArr = val
+        break
+      case 'speed':
+        this.sizeArr = val
+        break
+      case 'dur':
+        this.durArr = val
+        break
+      case 'size':
+        this.sizeArr = val
+        break
+      default:
+        break
+    }
   }
-  //   类型筛选
-  typeFilter(val) {
-    let data = this.filterData.filter((item) => {
-      return val.includes(item.type)
-    })
-    this.addToMarker(data)
-  }
-  //   航速筛选
-  speedFilter(val) {
-    let data = this.filterData.filter((item) => {
-      return val.includes(parseInt(item.speed))
-    })
-    this.addToMarker(data)
-  }
-  //   跟踪时长筛选
-  durFilter(val) {
-    console.log(val)
-    let data = null
-    if (val.length == 1 && val[0] == 3) {
-      data = this.filterData.filter((item) => {
-        return item.dur <= val[0]
-      })
-    } else if (val.length == 1) {
-      data = this.filterData.filter((item) => {
-        return item.dur <= val[0] && item.dur >= val[0] / 2
+  filterArrData() {
+    let dataArr = this.filterData
+
+    // 类型筛选
+    if (this.typeArr.length > 0) {
+      dataArr = dataArr.filter((item) => {
+        return val.includes(item.type)
       })
     } else {
-      data = this.filterData.filter((item) => {
-        return item.dur >= val[0] && item.dur <= val[val.length - 1]
+      dataArr = dataArr.filter((item) => {
+        return item.type != null
       })
     }
 
-    this.addToMarker(data)
+    // 航速筛选
+    if (this.speedArr.length > 0) {
+      dataArr = dataArr.filter((item) => {
+        return val.includes(parseInt(item.speed))
+      })
+    } else {
+      dataArr = dataArr.filter((item) => {
+        return item.speed != null
+      })
+    }
+
+    // 跟踪时长筛选
+    if (this.durArr.length > 0) {
+      if (val.length == 1 && val[0] == 3) {
+        dataArr = dataArr.filter((item) => {
+          return item.dur <= val[0]
+        })
+      } else if (val.length == 1) {
+        dataArr = dataArr.filter((item) => {
+          return item.dur <= val[0] && item.dur >= val[0] / 2
+        })
+      } else {
+        dataArr = dataArr.filter((item) => {
+          return item.dur >= val[0] && item.dur <= val[val.length - 1]
+        })
+      }
+    } else {
+      dataArr = dataArr.filter((item) => {
+        return item.dur != null
+      })
+    }
+
+    // 跟踪大小筛选
+    if (this.sizeArr.length > 0) {
+      dataArr = dataArr.filter((item) => {
+        return item.size <= val[val.length - 1] && item.size >= val[0]
+      })
+    } else {
+      dataArr = dataArr.filter((item) => {
+        return item.size != null
+      })
+    }
+
+    this.addToMarker(dataArr)
   }
+  //   类型筛选
+  // typeFilter(val) {
+  //   let data = this.filterData.filter((item) => {
+  //     return val.includes(item.type)
+  //   })
+  //   this.addToMarker(data)
+  // }
+  //   航速筛选
+  // speedFilter(val) {
+  //   let data = this.filterData.filter((item) => {
+  //     return val.includes(parseInt(item.speed))
+  //   })
+  //   this.addToMarker(data)
+  // }
+  //   跟踪时长筛选
+  // durFilter(val) {
+  //   console.log(val)
+  //   let data = null
+  //   if (val.length == 1 && val[0] == 3) {
+  //     data = this.filterData.filter((item) => {
+  //       return item.dur <= val[0]
+  //     })
+  //   } else if (val.length == 1) {
+  //     data = this.filterData.filter((item) => {
+  //       return item.dur <= val[0] && item.dur >= val[0] / 2
+  //     })
+  //   } else {
+  //     data = this.filterData.filter((item) => {
+  //       return item.dur >= val[0] && item.dur <= val[val.length - 1]
+  //     })
+  //   }
+
+  //   this.addToMarker(data)
+  // }
   //   目标大小筛选
   sizeFilter(val) {
     console.log(val)
